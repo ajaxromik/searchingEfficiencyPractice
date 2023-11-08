c7afbac27cbeff1ae559dd594cb36d6596d06d60
W Carr <carrwi96@students.rowan.edu>
2023-11-07 20:56:37 -0500

    added the list class

---
+++ AscendinglyOrderedStringList.java
@@ -0,0 +1,59 @@
+
+/**
+ * Purpose: Data Structure and Algorithms Lab 8 Problem 3
+ * Status: Complete and thoroughly tested
+ * Last update: 11/07/22
+ * Submitted:  11/07/22
+ * Comment: test suite and sample run attached
+ * Comment: I declare that this is entirely my own work
+ * @author: William Carr
+ * @version: 2023.11.07
+ */
+
+public class AscendinglyOrderedStringList {
+
+    private String []items;
+    private int numItems;
+
+    /**
+     * Creates an instance of the AscendinglyOrderedStringList class.
+     */
+    public AscendinglyOrderedStringList() {
+    	items = new String[3];
+    	numItems = 0;
+    }
+
+    /**
+     * Returns whether the list is empty
+     * @return true if the list is empty; otherwise, false
+     */
+	public boolean isEmpty(){
+
+	}
+
+	public int size(){
+
+	}
+
+	public void add(String item) throws ListIndexOutOfBoundsException{
+
+	}
+
+	public String get(int index) throws ListIndexOutOfBoundsException{
+
+	}
+
+	public String remove(int index) throws ListIndexOutOfBoundsException{
+
+	}
+
+	public int search(String key){
+
+	}
+
+	public void removeAll(){
+
+	}
+
+
+}
